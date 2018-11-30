e8a89f4bb99a47c36c1dc74eea42d1409691d90b
Melbourne97 <32548833+Melbourne97@users.noreply.github.com>
2018-11-30 15:27:47 -0500

    changed names

--- src/IMClient/ClientTest.java
+++ src/IMClient/ClientTest.java
@@ -6,7 +6,7 @@ public class ClientTest {
 
     public static void main(String[] args) {
         Client user;
-        user = new Client("157.62.180.88");
+        user = new Client("SERVER IP ADDRESS HERE");
         user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         user.startRunning();
     }
